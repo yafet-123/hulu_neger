846a9e2f067c60259eb6fb3f2b1d2b3016edd8c2
yafet-123 <yafetaddisu123@gmail.com>
2023-05-22 13:00:56 -0700

    Adding admin news page

---
+++ app/Admin/News/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function AdminUserHome() {
+  return (
+    <section className='w-full flex-center flex-col pt-24'>
+      user
+    </section>
+  )
+}
