dddb0e362578554c7ba3ab429c1719fe132de007
yafet-123 <yafetaddisu123@gmail.com>
2023-05-23 12:49:36 -0700

    editing the vertical navbar of the admin

--- app/Admin/User/page.tsx
+++ app/Admin/User/page.tsx
@@ -1,6 +1,39 @@
 import Image from 'next/image'
+import { PrismaClient } from '@prisma/client'
+
+
+
+
+async function fetchRepos() {
+  const prisma = new PrismaClient()
+  const response = await fetch(
+    // fetch from our code repository
+    'https://api.github.com/users/yafet-123/repos',
+    {   
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+  const repos = await response.json(); // change file response json file
+  return repos;
+}
 
 export default function AdminUserHome() {
+  async function main() {
+  const users = await prisma.User.findMany({orderBy : {ModifiedDate:'desc'}});
+    console.log(users)
+  }
+
+  main()
+    .then(async () => {
+      await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+      console.error(e)
+      await prisma.$disconnect()
+      process.exit(1)
+    })
   return (
     <section className='w-full flex-center flex-col pt-24'>
       user
--- components/Admin/Common/VerticalNavbar.tsx
+++ components/Admin/Common/VerticalNavbar.tsx
@@ -1,42 +1,31 @@
 "use client"
-import React, {useState,useEffect} from "react";
+import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import Link from 'next/link'
-import { FaBlogger,FaBlog } from "react-icons/fa"
-import { AiOutlineClose, AiOutlineMail, AiOutlineMenu, AiFillDashboard, AiOutlineUser, AiOutlineFolderAdd, AiOutlineHtml5 } from "react-icons/ai";
-import { DiJavascript1, DiPython } from "react-icons/di"
-import { GiArtificialIntelligence, GiArtificialHive, GiNewspaper} from "react-icons/gi"
-import { SiCss3 } from 'react-icons/si'
-import { MdOutlineCategory, MdLocationOn } from "react-icons/md";
-import { BsNewspaper, BsFiletypeCss } from 'react-icons/bs'
-import Link from 'next/link'
-import { FiLogOut } from "react-icons/fi"
-import { BiDownArrow, BiNews } from "react-icons/bi"
-import { BsDisplay, BsFunnelFill } from 'react-icons/bs'
+import React, {useState,useEffect} from "react";
 
 
 export default function VerticalNavbar(){
     const SideBarList = [
-        { link: "/Admin", icon: <AiFillDashboard size={25}/>, name: "Dashboard",},
-        { link: "/Admin/User", icon: <AiOutlineUser size={25}/>, name: "User",},
-        { link: "/Admin/Job/Add", icon: <AiOutlineFolderAdd size={25}/>, name: "Add Job" },
-        { link: "/Admin/Job/Display", icon: <BsDisplay size={25}/>, name: "Display Job" },
-        { link: "/Admin/Job/Category", icon: <MdOutlineCategory size={25}/>, name: "Category" },
-        { link: "/Admin/Job/Location", icon: <MdLocationOn size={25}/>, name: "Location" },
-        { link: "/Admin/NewsCategory", icon: <BiNews size={25}/>, name: "News Category" },
-        { link: "/Admin/News", icon: <GiNewspaper size={25}/>, name: "News" },
-        { link: "/Admin/EntertainmentCategory", icon: <BsNewspaper size={25}/>, name: "Entertainment Category" },
-        { link: "/Admin/Entertainment", icon: <BsFunnelFill size={25}/>, name: "Entertainment" },
-        { link: "/Admin/HTmlCourse", icon: <AiOutlineHtml5 size={25}/>, name: "HTML Course" },
-        { link: "/Admin/CSSCourse", icon: <SiCss3 size={25}/>, name: "CSS Course" },
-        { link: "/Admin/JavascriptCourses", icon: <DiJavascript1 size={25}/>, name: "JavaScript Course" },
-        { link: "/Admin/PythonCourses", icon: <DiPython size={25}/>, name: "Python Course" },
-        { link: "/Admin/BlogsCategory", icon: <FaBlog size={25}/>, name: "Blogs Category" },
-        { link: "/Admin/Blogs", icon: <FaBlogger size={25}/>, name: "Blogs" },
-        { link: "/Admin/AiSearchCategory", icon: <GiArtificialHive size={25}/>, name: "AiSearch Category" },
-        { link: "/Admin/AiSearch", icon: <GiArtificialIntelligence size={25}/>, name: "AiSearch" },
+        { link: "/Admin", name: "Dashboard",},
+        { link: "/Admin/User",  name: "User",},
+        { link: "/Admin/Job/Add",  name: "Add Job" },
+        { link: "/Admin/Job/Display",  name: "Display Job" },
+        { link: "/Admin/Category",  name: "Category" },
+        { link: "/Admin/Location",  name: "Location" },
+        { link: "/Admin/NewsCategory",  name: "News Category" },
+        { link: "/Admin/News",  name: "News" },
+        { link: "/Admin/EntertainmentCategory",  name: "Entertainment Category" },
+        { link: "/Admin/Entertainment",  name: "Entertainment" },
+        { link: "/Admin/HTmlCourse",  name: "HTML Course" },
+        { link: "/Admin/CSSCourse",  name: "CSS Course" },
+        { link: "/Admin/JavascriptCourses",  name: "JavaScript Course" },
+        { link: "/Admin/PythonCourses",  name: "Python Course" },
+        { link: "/Admin/BlogsCategory",  name: "Blogs Category" },
+        { link: "/Admin/Blogs",  name: "Blogs" },
+        { link: "/Admin/AiSearchCategory",  name: "AiSearch Category" },
+        { link: "/Admin/AiSearch",  name: "AiSearch" },
     ];
-	const router = useRouter();
+    const router = useRouter();
     const [sideBar , setsideBar] = useState(false);
     const handleSideBar = () => {
         setsideBar(!sideBar);
@@ -44,39 +33,37 @@ export default function VerticalNavbar(){
     const path = router.pathname
 
 	return(
-            <div className={`flex h-full sticky top-0 bottom-0 ${sideBar ? "w-16 lg:w-28" : "w-16 lg:w-96"} pt-24`}>
-                <nav className="w-full h-screen flex flex-col lg:px-4 bg-transparent dark:bg-[#02201D] scroll_width">
-                    <div className="flex justify-between ml-2 lg:ml-5">
-                        <h1 className={`text-2xl font-bold text-black dark:text-white ${sideBar ? "hidden" : "hidden lg:flex"}`}>Admin Page</h1>
-                        <button 
-                            onClick={handleSideBar} 
-                            className={`flex text-black dark:text-white hover:text-slate-800 focus:outline-none ${ sideBar ? "flex justify-center items-center" : ""} `}
-                        >
-                            <AiOutlineMenu size={35} />
-                        </button>
-                    </div>
-                    <div className="mt-10">
-                        <ul>
-                            {SideBarList.map((side, index) => (
-                                <li className="mb-5" key={index}>
-                                    <Link 
-                                        href={side.link}
-                                        className={ side.link == path ? "lg:w-full bg-white flex items-center px-4 py-2 lg:py-4 text-xs lg:text-sm text-black rounded-xl":
-                                        "lg:w-full flex items-center px-4 py-2 lg:py-4 text-xs lg:text-sm text-black hover:text-white dark:text-white hover:bg-[#009688] rounded-xl" }
-                                    >
-                                            <span className="text-xs lg:text-lg">{side.icon}</span>
-                                            <span className={`ml-0 lg:ml-4 font-semibold ${sideBar ? 'hidden' : 'hidden lg:flex' } `}>
-                                                {side.name}
-                                            </span>
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className="mt-auto flex flex-col">
+        <div className={`flex h-full sticky top-0 bottom-0 ${sideBar ? "w-16 lg:w-28" : "w-16 lg:w-96"} pt-24`}>
+            <nav className="w-full h-screen flex flex-col py-8 lg:px-4 dark:bg-[#02201D] scroll_width">
+                <div className="flex justify-between ml-2 lg:ml-5">
+                    <h1 className={`text-2xl font-bold text-black dark:text-white ${sideBar ? "hidden" : "hidden lg:flex"}`}>Admin Page</h1>
+                    <button 
+                        onClick={handleSideBar} 
+                        className={`flex text-black dark:text-white hover:text-slate-800 focus:outline-none ${ sideBar ? "flex justify-center items-center" : ""} `}
+                    >
                         
-                    </div>
-                </nav>
-            </div>
+                    </button>
+                </div>
+
+                <div className="mt-10">
+                    <ul>
+                        {SideBarList.map((side, index) => (
+                            <li className="mb-5" key={index}>
+                                <Link
+                                    href={side.link}
+                                    className={ side.link == path ? "lg:w-full bg-white flex items-center px-4 py-2 lg:py-4 text-xs lg:text-sm text-black rounded-xl":
+                                    "lg:w-full flex items-center px-4 py-2 lg:py-4 text-xs lg:text-sm text-black hover:text-white dark:text-white hover:bg-[#009688] rounded-xl" }
+                                >
+                                    <span className="text-xs lg:text-lg">{side.icon}</span>
+                                    <span className={`ml-0 lg:ml-4 font-semibold ${sideBar ? 'hidden' : 'hidden lg:flex' } `}>
+                                        {side.name}
+                                    </span>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </nav>
+        </div>
 	)
 }
