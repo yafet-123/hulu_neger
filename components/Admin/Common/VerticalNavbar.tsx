5a5c2ff5dc7bd9cf50893c0ad985b07b44565cf6
yafet-123 <yafetaddisu123@gmail.com>
2023-05-24 10:16:32 -0700

    editing the admin vertical navbar icon

--- components/Admin/Common/VerticalNavbar.tsx
+++ components/Admin/Common/VerticalNavbar.tsx
@@ -5,29 +5,31 @@ import React, {useState,useEffect} from "react";
 
 // font awesome icon
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/pro-solid-svg-icons'
+import { faBars,faHouse,faUser,faLocationCrosshairs, faNewspaper, faTypewriter,faCameraMovie,
+    faJavaScript,faCode,faCSS3Logo,faBlog , faPython} from '@fortawesome/free-solid-svg-icons'
 
 export default function VerticalNavbar(){
     const SideBarList = [
-        { link: "/Admin", name: "Dashboard",},
-        { link: "/Admin/User",  name: "User",},
+        { link: "/Admin", icons:<FontAwesomeIcon icon={faHouse} beat size="xl" />, name: "Dashboard",},
+        { link: "/Admin/User", icons:<FontAwesomeIcon icon={faUser} beat size="xl"/>,  name: "User",},
         { link: "/Admin/Job/Add",  name: "Add Job" },
         { link: "/Admin/Job/Display",  name: "Display Job" },
         { link: "/Admin/Category",  name: "Category" },
-        { link: "/Admin/Location",  name: "Location" },
-        { link: "/Admin/NewsCategory",  name: "News Category" },
-        { link: "/Admin/News",  name: "News" },
+        { link: "/Admin/Location",icons:<FontAwesomeIcon icon={faLocationCrosshairs} beat size="xl" />,  name: "Location" },
+        { link: "/Admin/NewsCategory", name: "News Category" },
+        { link: "/Admin/News", icons:<FontAwesomeIcon icon={faNewspaper} beat size="xl" />,  name: "News" },
         { link: "/Admin/EntertainmentCategory",  name: "Entertainment Category" },
-        { link: "/Admin/Entertainment",  name: "Entertainment" },
-        { link: "/Admin/HTmlCourse",  name: "HTML Course" },
-        { link: "/Admin/CSSCourse",  name: "CSS Course" },
-        { link: "/Admin/JavascriptCourses",  name: "JavaScript Course" },
-        { link: "/Admin/PythonCourses",  name: "Python Course" },
-        { link: "/Admin/BlogsCategory",  name: "Blogs Category" },
+        { link: "/Admin/Entertainment", name: "Entertainment" },
+        { link: "/Admin/HTmlCourse",icons:<FontAwesomeIcon icon={faCode} beat size="xl" />,  name: "HTML Course" },
+        { link: "/Admin/CSSCourse", name: "CSS Course" },
+        { link: "/Admin/JavascriptCourses", name: "JavaScript Course" },
+        { link: "/Admin/PythonCourses", name: "Python Course" },
+        { link: "/Admin/BlogsCategory",icons:<FontAwesomeIcon icon={faBlog} beat size="xl" />,  name: "Blogs Category" },
         { link: "/Admin/Blogs",  name: "Blogs" },
         { link: "/Admin/AiSearchCategory",  name: "AiSearch Category" },
         { link: "/Admin/AiSearch",  name: "AiSearch" },
     ];
+   
     const router = useRouter();
     const [sideBar , setsideBar] = useState(false);
     const handleSideBar = () => {
@@ -44,7 +46,7 @@ export default function VerticalNavbar(){
                         onClick={handleSideBar} 
                         className={`flex text-black dark:text-white hover:text-slate-800 focus:outline-none ${ sideBar ? "flex justify-center items-center" : ""} `}
                     >
-                        <FontAwesomeIcon icon={faBars} flip />
+                        <FontAwesomeIcon icon={faBars} flip size="2xl" />
                     </button>
                 </div>
 
@@ -57,7 +59,7 @@ export default function VerticalNavbar(){
                                     className={ side.link == path ? "lg:w-full bg-white flex items-center px-4 py-2 lg:py-4 text-xs lg:text-sm text-black rounded-xl":
                                     "lg:w-full flex items-center px-4 py-2 lg:py-4 text-xs lg:text-sm text-black hover:text-white dark:text-white hover:bg-[#009688] rounded-xl" }
                                 >
-                                    <span className="text-xs lg:text-lg">{side.icon}</span>
+                                    {side.icons}
                                     <span className={`ml-0 lg:ml-4 font-semibold ${sideBar ? 'hidden' : 'hidden lg:flex' } `}>
                                         {side.name}
                                     </span>
