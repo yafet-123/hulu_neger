435ca3b029d5e7962704b7e976407741f851f4e8
yafet-123 <yafetaddisu123@gmail.com>
2023-05-25 12:44:41 -0700

    adding prisma client

--- app/Admin/User/page.tsx
+++ app/Admin/User/page.tsx
@@ -1,9 +1,5 @@
 import Image from 'next/image'
 import { PrismaClient } from '@prisma/client'
-
-
-
-
 export default function AdminUserHome() {
   (
     <section className='w-full flex-center flex-col pt-24'>
