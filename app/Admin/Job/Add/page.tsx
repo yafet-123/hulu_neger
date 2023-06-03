44f45fb345dcbb390876c0d2f6197ab419809e52
yafet-123 <yafetaddisu123@gmail.com>
2023-06-03 00:36:37 -0700

    adding the job add

--- app/Admin/Job/Add/page.tsx
+++ app/Admin/Job/Add/page.tsx
@@ -1,9 +1,93 @@
+"use client";
 import Image from 'next/image'
+import Form from "@/components/Admin/Job/Form";
+import Display from "@/components/Admin/Job/Display";
+import { useState,useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { usePathname, useRouter } from "next/navigation";
 
-export default function AdminHome() {
+
+const JobCardList = ({ data }) => {
   return (
-    <section className='w-full flex-center flex-col pt-24'>
-      Add Job
+    <div className='mt-16 prompt_layout'>
+      {data.map((data) => (
+        <Display
+          key={data._id}
+          Job={data}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default function JobHome() {
+  const router = useRouter();
+  const [submitting, setIsSubmitting] = useState(false);
+  const [allJob, setAllJob] = useState([]);
+  const [allJobCategory, setAllJobCategory] = useState([]);
+  const [job, setJob] = useState({ CompanyName : "", Image:"", JobsName:"", CareerLevel:"", Salary:"", Descreption:"", shortDescreption:"", LocationId:[], categoryId:[]  });
+  const { data: session } = useSession();
+  const createJob = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    try {
+      const response = await fetch("/api/Job//Add", {
+        method: "POST",
+        body: JSON.stringify({
+          CompanyName:job.CompanyName,
+          Image:job.imageData,
+          JobsName:job.JobsName,
+          CareerLevel:job.CareerLevel,
+          Salary:job.Salary,
+          Descreption:job.Description,
+          shortDescreption:job.shortDescription,
+          DeadLine:job.DeadLine,
+          categoryId:job.categoryId,
+          LocationId:job.LocationId,
+          user_id: session?.user.id,
+        }),
+      });
+      if (response.ok) {
+        router.push("/Admin");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  const fetchJob = async () => {
+    const response = await fetch("/api/Job");
+    const data = await response.json();
+
+    setAllJob(data);
+  };
+
+  const fetchJobCategory = async () => {
+    const response = await fetch("/api/Job/Category");
+    const data = await response.json();
+
+    setAllJobCategory(data);
+  };
+
+  useEffect(() => {
+    fetchJob();
+    fetchJobCategory();
+  }, []);
+  return (
+    <section className='w-full box-border lg:pt-24'>
+      <Form
+        type='Create'
+        typeof="Job"
+        Job={Job}
+        setJob={setJob}
+        categories={allJobCategory}
+        submitting={submitting}
+        handleSubmit={createJob}
+      />
+
+      <JobCardList data={allJob} />
     </section>
   )
 }
