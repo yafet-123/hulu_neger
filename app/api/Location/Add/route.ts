a23e027f383f0b8bf30322457958233c4dfb6661
yafet-123 <yafetaddisu123@gmail.com>
2023-06-03 02:06:32 -0700

    adding the location api

---
+++ app/api/Location/Add/route.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/utils/db.server";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+export const POST = async (req: NextApiRequest, res: NextApiResponse) => {
+  const {LocationName , user_id, Image} = await req.json();
+  try {
+    console.log(CategoryName);
+    const data = await prisma.Location.create({
+      data: {
+        LocationName: LocationName,
+        Image:Image,
+        user_id: Number(user_id),
+      },
+    });
+    console.log(data);
+    return new Response(JSON.stringify(data), { status: 201 });
+  } catch (error) {
+    return new Response("Failed to create a new Job Category", { status: 500 });
+  }
+};
