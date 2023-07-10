8538f002b31f3dfe909f11ab53116790bacd1a9e
yafet-123 <yafetaddisu123@gmail.com>
2023-07-10 06:56:36 -0700

    editing the et display component

--- app/api/AiSearch/Add/route.ts
+++ app/api/AiSearch/Add/route.ts
@@ -1,11 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "@/utils/db.server";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
- 
-export const POST = async (req,res) => {
-  const { Header, description, like, link, service, categoryId, user_id } =
-    await req.json();
+
+export const POST = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { Header, description, like, link, service, categoryId, user_id } = await req.json();
 
   try {
     let createAiSearchCategory = [];
